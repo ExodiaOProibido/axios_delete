a59c504d07060ad5b7181060420428f0ba776afe
ExodiaOProibido <hugo0711.oliveira@gmail.com>
2025-09-12 21:16:49 -0300

    commit

---
+++ axios_delete/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, FlatList, StyleSheet } from "react-native";
+import axios from "axios";
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+
+  const API = "http://10.110.12.26:3000/users";
+
+  // Buscar usuários
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(API);
+      setUsers(response.data);
+    } catch (error) {
+      console.error("ERROR GET:", error.message);
+    }
+  };
+
+  // Deletar usuário
+  const deleteUser = async (id) => {
+    try {
+      await axios.delete(`${API}/${id}`);
+      setUsers((prevUsers) => prevUsers.filter((u) => u.id !== id));
+    } catch (error) {
+      console.error("ERROR DELETE:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>DELETE - Remover Usuário</Text>
+
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.userItem}>
+            <Text>{item.name} - {item.email}</Text>
+            <Button title="DEL" onPress={() => deleteUser(item.id)} />
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20, marginTop: 40 },
+  title: { fontSize: 22, fontWeight: "bold", marginBottom: 10 },
+  userItem: { marginTop: 10 }
+});
